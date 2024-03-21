5fd6d92da2d29d01271637f7fd05261e61701ec9
sleekcharly <c_ukasoanya@yahoo.com>
2024-03-21 18:36:24 +0100

    set up stepzen for grapghql server management

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,3 +1,6 @@
+import { getClient } from '@/apollo-client';
+import fetchWeatherQuery from '@/graphql/queries/fetchWeatherQueries';
+
 type Props = {
   params: {
     city: string;
@@ -6,7 +9,24 @@ type Props = {
   };
 };
 
-function WeatherPage({ params: { city, lat, long } }: Props) {
+async function WeatherPage({ params: { city, lat, long } }: Props) {
+  const client = getClient();
+
+  const { data } = await client.query({
+    query: fetchWeatherQuery,
+    variables: {
+      current:
+        'temperature_2m,is_day,weather_code,wind_speed_10m,wind_direction_10m',
+      longitude: long,
+      latitude: lat,
+      timezone: 'auto',
+    },
+  });
+
+  const results: Root = data.myQuery;
+
+  console.log(results);
+
   return (
     <div>
       WeatherPage: {city} {lat} {long}
---
+++ graphql/queries/fetchWeatherQueries.ts
@@ -0,0 +1,101 @@
+import { gql } from '@apollo/client';
+
+const fetchWeatherQuery = gql`
+  query myQuery(
+    $current: String
+    $daily: String = "weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max"
+    $hourly: String = "temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,wind_gusts_10m"
+    $latitude: String!
+    $longitude: String!
+    $timezone: String!
+  ) {
+    myQuery(
+      current: $current
+      daily: $daily
+      hourly: $hourly
+      latitude: $latitude
+      longitude: $longitude
+      timezone: $timezone
+    ) {
+      current {
+        interval
+        is_day
+        temperature_2m
+        time
+        weather_code
+        wind_direction_10m
+        wind_speed_10m
+      }
+      current_units {
+        interval
+        is_day
+        temperature_2m
+        time
+        weather_code
+        wind_direction_10m
+        wind_speed_10m
+      }
+      daily {
+        apparent_temperature_max
+        apparent_temperature_min
+        sunrise
+        sunset
+        temperature_2m_max
+        temperature_2m_min
+        time
+        uv_index_clear_sky_max
+        uv_index_max
+        weather_code
+      }
+      daily_units {
+        apparent_temperature_max
+        apparent_temperature_min
+        sunrise
+        sunset
+        temperature_2m_max
+        temperature_2m_min
+        time
+        uv_index_clear_sky_max
+        uv_index_max
+        weather_code
+      }
+      elevation
+      generationtime_ms
+      hourly {
+        apparent_temperature
+        dew_point_2m
+        precipitation
+        precipitation_probability
+        rain
+        relative_humidity_2m
+        showers
+        snow_depth
+        snowfall
+        temperature_2m
+        time
+        wind_gusts_10m
+      }
+      hourly_units {
+        apparent_temperature
+        dew_point_2m
+        precipitation
+        precipitation_probability
+        rain
+        relative_humidity_2m
+        showers
+        snow_depth
+        snowfall
+        temperature_2m
+        time
+        wind_gusts_10m
+      }
+      latitude
+      longitude
+      timezone
+      timezone_abbreviation
+      utc_offset_seconds
+    }
+  }
+`;
+
+export default fetchWeatherQuery;
